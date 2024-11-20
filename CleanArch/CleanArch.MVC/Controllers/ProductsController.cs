48fece5a402cba8b9249c8e583e045f9a26c5d67
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-11-20 14:32:02 -0300

    Finalização do projeto inicial e básico do DDD

---
+++ CleanArch/CleanArch.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            //var result = await 
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id, Name, Description, Price")])
+        {
+
+        }*/
+
+        /*[HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var productVM = await 
+        }*/
+    }
+}
