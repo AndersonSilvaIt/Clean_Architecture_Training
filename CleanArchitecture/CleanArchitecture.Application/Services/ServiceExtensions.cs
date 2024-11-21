d9095e55e685ff788bf2071ca06e1dcb19db86f6
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-11-21 13:04:22 -0300

    Implementado validação de usuário ao salvar

--- CleanArchitecture/CleanArchitecture.Application/Services/ServiceExtensions.cs
+++ CleanArchitecture/CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using CleanArchitecture.Application.Shared.Behavior;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -11,6 +13,8 @@ namespace CleanArchitecture.Application.Services
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         }
     }
 }
