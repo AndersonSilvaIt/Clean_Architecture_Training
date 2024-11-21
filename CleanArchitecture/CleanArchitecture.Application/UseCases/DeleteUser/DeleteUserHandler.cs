e10aea96c7d50589bda551b0a32b8d03305ffa55
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-11-21 13:36:28 -0300

    Implementado o CRUD final

---
+++ CleanArchitecture/CleanArchitecture.Application/UseCases/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CleanArchitecture.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchitecture.Application.UseCases.DeleteUser
+{
+    public sealed class DeleteUserHandler : IRequestHandler<DeleteUserRequest, DeleteUserResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteUserHandler(IUserRepository userRepository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<DeleteUserResponse> Handle(DeleteUserRequest command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.Get(command.Id, cancellationToken);
+
+            if (user == null) return default;
+
+            _userRepository.Delete(user);
+
+            await _unitOfWork.Commit(cancellationToken);
+
+            return _mapper.Map<DeleteUserResponse>(user);
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Application/UseCases/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using CleanArchitecture.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchitecture.Application.UseCases.UpdateUser
+{
+    public sealed class UpdateUserHandler : IRequestHandler<UpdateUserRequest, UpdateUserResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateUserHandler(IUserRepository userRepository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<UpdateUserResponse> Handle(UpdateUserRequest command, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.Get(command.Id, cancellationToken);
+
+            if (user == null) return default;
+
+            user.Email = command.Email;
+            user.Name = command.Email;
+
+            _userRepository.Update(user);
+
+            await _unitOfWork.Commit(cancellationToken);
+
+            return _mapper.Map<UpdateUserResponse>(user);
+        }
+    }
+}
